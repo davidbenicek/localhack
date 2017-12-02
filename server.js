b174d3e5c94aaaf07239187c31061ab1b922e1cb
Sebastian Köhler <sebastiankoehler2@gmx.net>
2017-12-02 14:55:04 +0000

    added imageHandler service

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 'use strict'
-//Imports
+//Lib imports
 var express = require('express');
 var bodyParser = require('body-parser');
+
+// Our imports
+
+var imageRecognitionService = require("./services/imageRecognitionService.js");
+var imageHandler = require("./services/imageHandler.js");
 
 //Start server
 var app = express();
@@ -16,5 +21,20 @@ app.get('/test', function (req, res) {
     res.send({"text": "test"});
 });
 
+app.post('/image', function (req, res) {
+    const name = req.body.name;
+    const image = req.body.image;
+    try {
+       console.log("image");
+        imageHandler.saveImage(name, image);
+       // var result = imageRecognitionService.getWebData(name);
+
+    } catch (err) {
+       console.log("zoooo");
+    }
+    res.send("");
+});
+
 app.listen(port);
 console.log("Backend server running on http://localhost:1200/")
+
